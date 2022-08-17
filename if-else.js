56a34adf80f846f8d2e8123e00626030e405a241
Gurulakshmi-123 <gurulakshmi2897@gmail.com>
2022-08-17 15:32:23 +0530

    if condition statements

---
+++ if-else.js
@@ -0,0 +1,29 @@
+var a=20;  
+if(a>10){  
+console.log("value of a is greater than 10");  //JavaScript If statement
+} 
+
+
+var a=20;  
+if(a%2==0){  
+console.log("a is even number");  
+}  
+else{  
+console.log("a is odd number");   //JavaScript If...else Statement
+}  
+
+
+
+var a=20;  
+if(a==10){  
+console.log("a is equal to 10");  
+}  
+else if(a==15){  
+console.log("a is equal to 15");  
+}  
+else if(a==20){  
+console.log("a is equal to 20");  
+}  
+else{  
+console.log("a is not equal to 10, 15 or 20");  //javaScript If...else if statement
+}  
